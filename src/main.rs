7e20087e522041a78dddf3a6aa04979ef118728b
jmdoucette <jmdoucette41@gmail.com>
2021-07-05 14:58:27 -0500

    testing ci

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod recurrence_solution;
 mod test_utils;
 
 fn main() {
+    println!("testing");
     let p = Polynomial::new(vec![-1.0,-1.0,1.0]);
     println!("{}, degree: {}, roots: {:?}", p, p.degree(), p.roots());
 
