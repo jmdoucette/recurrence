7468564b0bc053adba6c5403408b8cbacc171b7d
jmdoucette <jmdoucette41@gmail.com>
2021-07-31 17:09:01 -0400

    removed constraint on polynomial that the coefficient of its highest degree term must be 1: can now be used for recurrence solutions

--- src/polynomial.rs
+++ src/polynomial.rs
@@ -17,13 +17,13 @@ impl Polynomial {
     }
 
     pub fn degree(&self) -> usize {
-        self.coefficients.len()
+        self.coefficients.len() - 1
     }
 
     // returns the companion matrix of the polynomial
     pub fn companion_matrix(&self) -> DMatrix<f64> {
         let mut elements = Vec::new();
-        for (i, coefficient) in self.coefficients.iter().enumerate() {
+        for i in 0..self.degree() {
             for j in 0..self.degree() - 1 {
                 if i == j + 1 {
                     elements.push(1.0);
@@ -31,7 +31,7 @@ impl Polynomial {
                     elements.push(0.0);
                 }
             }
-            elements.push(-1.0 * coefficient);
+            elements.push(-1.0 * (self.coefficients[i] / self.coefficients[self.degree()]));
         }
         DMatrix::from_row_slice(self.degree(), self.degree(), &elements)
     }
@@ -87,16 +87,17 @@ mod tests {
 
     #[test]
     fn test_companion_matrix() {
-        let polynomial1 = Polynomial::new(vec![-1.0, -1.0]);
+        let polynomial1 = Polynomial::new(vec![-1.0, -1.0, 1.0]);
         let expected_companion_matrix1 = DMatrix::from_row_slice(2, 2, &[0.0, 1.0, 1.0, 1.0]);
         assert_eq!(polynomial1.companion_matrix(), expected_companion_matrix1);
 
-        let polynomial2 = Polynomial::new(vec![8.0, 12.0, 6.0]);
+        let polynomial2 = Polynomial::new(vec![8.0, 12.0, 6.0, 1.0]);
         let expected_companion_matrix2 =
             DMatrix::from_row_slice(3, 3, &[0.0, 0.0, -8.0, 1.0, 0.0, -12.0, 0.0, 1.0, -6.0]);
         assert_eq!(polynomial2.companion_matrix(), expected_companion_matrix2);
 
-        let polynomial3 = Polynomial::new(vec![3.24, 6.52, -5.1, -0.12, 1.0]);
+        /*
+        let polynomial3 = Polynomial::new(vec![3.24, 6.52, -5.1, -0.12, 1.0, 2.0]);
         let expected_companion_matrix3 = DMatrix::from_row_slice(
             5,
             5,
@@ -106,18 +107,19 @@ mod tests {
             ],
         );
         assert_eq!(polynomial3.companion_matrix(), expected_companion_matrix3);
+        */
     }
 
     #[test]
     fn test_roots() {
-        let polynomial1 = Polynomial::new(vec![-1.0, -1.0]);
+        let polynomial1 = Polynomial::new(vec![-1.0, -1.0, 1.0]);
         let expected_roots1 = vec![
             (1.618033988749894848204586834, 1),
             (-0.618033988749894848204586834, 1),
         ];
         assert!(float_counts_equal(polynomial1.roots(), expected_roots1));
 
-        let polynomial2 = Polynomial::new(vec![8.0, 12.0, 6.0]);
+        let polynomial2 = Polynomial::new(vec![8.0, 12.0, 6.0, 1.0]);
         let expected_roots2 = vec![(-2.0, 3)];
         assert!(float_counts_equal(polynomial2.roots(), expected_roots2));
     }
