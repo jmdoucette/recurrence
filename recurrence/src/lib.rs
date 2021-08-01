064ac0e069a60fba433071cb5f928fef8c3db80e
jmdoucette <jmdoucette41@gmail.com>
2021-07-31 23:28:59 -0400

    reorganized

---
+++ recurrence/src/lib.rs
@@ -0,0 +1,8 @@
+mod polynomial;
+mod recurrence_relation;
+mod recurrence_solution;
+mod utilities;
+
+pub use polynomial::Polynomial;
+pub use recurrence_relation::RecurrenceRelation;
+pub use recurrence_solution::RecurrenceSolution;
