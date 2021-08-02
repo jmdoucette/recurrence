b4334f257a7f3aa38fc8e688315a1a5962a0992b
jmdoucette <jmdoucette41@gmail.com>
2021-08-02 16:19:35 -0400

    cleanup

--- web-app/src/main.rs
+++ web-app/src/main.rs
@@ -1,8 +1,12 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+
+use recurrence::RecurrenceRelation;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> String {
+    let recurrence_solution = RecurrenceRelation::new(vec![0.0, 1.0], vec![1.0, 1.0]).solve();
+    format!("solution to fibonacci: {:?}", recurrence_solution)
 }
 
 #[launch]
